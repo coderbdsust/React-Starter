19d0486b5f2aa5988191c8228effa75f1a622a8d
Biswajit Debnath <biswajit@Tests-MacBook-Pro.local>
2018-12-28 00:04:38 +0600

    Very Basic React App

--- src/App.js
+++ src/App.js
@@ -3,22 +3,23 @@ import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
+
+  constructor(props){
+    super(props);
+    this.state = {counter:0};
+  }
+
   render() {
     return (
-      <div className="App">
+      <div className="App" data-test="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
+            <h1 data-test='counter-display'>
+              This counter is incrementing {this.state.counter}
+            </h1>
+          <button data-test="inc-button" onClick={()=>this.setState({counter : this.state.counter+1})}> 
+            Increment Counter
+          </button>   
         </header>
       </div>
     );
--- src/App.test.js
+++ src/App.test.js
@@ -1,9 +1,36 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import Enzyme, {shallow} from 'enzyme';
+import EnzymeAdapter from 'enzyme-adapter-react-16';
 import App from './App';
 
+Enzyme.configure({adapter:new EnzymeAdapter()});
+const setup = (props={},state = null) => {
+  return shallow(<App {...props} />);
+}
+
+const findByAttr = (wrapper,val)=>{
+  return wrapper.find(`[data-test="${val}"]`);
+}
 it('renders without crashing', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(<App />, div);
-  ReactDOM.unmountComponentAtNode(div);
+  const wrapper  = setup();
+  const appComp = findByAttr(wrapper,'App');
+  expect(appComp.length).toBe(1);
+});
+
+it('renders increment button ', () => {
+  const wrapper  = setup();
+  const button = findByAttr(wrapper,'inc-button');
+  expect(button.length).toBe(1);
+});
+
+it('counter display working', () => {
+  const wrapper  = setup();
+  const counterDisplay = findByAttr(wrapper,'counter-display');
+  expect(counterDisplay.length).toBe(1);
+});
+
+it('counter display increment working', () => {
+  const wrapper  = setup();
+  const counterDisplay =findByAttr(wrapper,'counter-display');
+  expect(counterDisplay.length).toBe(1);
 });
